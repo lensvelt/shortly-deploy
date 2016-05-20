9c7a4ef6e1ea21c1d43ea16318d8ac37ea0cc269
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 20:52:54 -0700

    Add userSchema methods to schema for hashing/comparison

--- app/models/link.js
+++ app/models/link.js
@@ -12,21 +12,13 @@ var linksSchema = new Schema({
   visits: Number
 });
 
+// linksSchema.methods.shorten = function () {
+//   var shasum = crypto.createHash('sha1');
+//   shasum.update(model.get('url'));
+//   model.set('code', shasum.digest('hex').slice(0, 5)); 
+// };
+
+
 var Link = mongoose.model('Link', linksSchema);
 
 module.exports = Link;
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
--- app/models/user.js
+++ app/models/user.js
@@ -9,22 +9,22 @@ var usersSchema = new Schema({
   password: { type: String, required: true }
 });
 
+
+usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+},
+
+usersSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      this.save();
+    });
+};
+
 var User = mongoose.model('User', usersSchema);
-
-
-
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function() {
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
 
 module.exports = User;
